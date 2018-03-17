d2a18e4ceefc80614b218230978ea11ee74dffca
Pavle Goloskokovic <pavle@prunegames.com>
2018-03-16 23:53:54 -0600

    dots pulsing, new colors

--- js/game.js
+++ js/game.js
@@ -151,6 +151,14 @@ function update (time, delta)
 
         var factor = 1 + ((dot.y - player.y)+window.innerHeight/3*2)/window.innerHeight;
 
+        dot.graphics.clear();
+
+        dot.graphics.fillStyle(dot.color, 1.0);
+        dot.graphics.lineStyle(2, dot.color, -2*dot.transmitSize/PLAYER_SIZE - 3);
+
+        dot.graphics.fillCircle(-PLAYER_SIZE/2, -PLAYER_SIZE/2, PLAYER_SIZE);
+        dot.graphics.strokeCircle(-PLAYER_SIZE/2, -PLAYER_SIZE/2, dot.transmitSize);
+
         dot.graphics.x = (dot.x - player.x) * factor;
         dot.graphics.y = (dot.y - player.y) * factor;
         dot.graphics.depth = dot.graphics.y;
@@ -223,25 +231,69 @@ function createDot () {
         y: Math.random() * 600,
         graphics: this.add.graphics(),
         person: this.add.image(0, 0, 'person'),
-
+        //color: selectColor(Math.floor(Math.random()*113), 113),
+        color: getRandomColor(),
         visit: function () {
 
-        }
+        },
+        transmitSize: PLAYER_SIZE
     };
 
-    dot.graphics.fillStyle(selectColor(Math.floor(Math.random()*111), 111), 1.0);
-
-    dot.graphics.fillCircle(-PLAYER_SIZE/2, -PLAYER_SIZE/2, PLAYER_SIZE);
+    dot.transmitTween = this.tweens.add({
+        targets: dot,
+        duration: 1000,
+        transmitSize: PLAYER_SIZE * 1.5,
+        //delay: Math.random() * 2,
+        //ease: 'Sine.easeInOut',
+        repeat: -1,
+        yoyo: false
+    });
 
     dot.person.visible = false;
 
     return dot;
 }
 
+var colors = [
+    0xf48c41,
+    0xf16a45,
+    0xe64560,
+    0xf32883,
+    0xe87295,
+    0xf55b7a,
+    0xe3449b,
+    0x42bd9e,
+    0x2dd5b9,
+    0x2dd5b9,
+    0x76e5e9,
+    0x16cde9,
+    0x41adb1,
+    0x40b7cf,
+    0x33b3e2,
+    0x345dcd,
+    0x5896cd,
+    0x413cd3,
+    0x465ee7,
+    0x6663ed,
+    0x7f68e4,
+    0x964cea,
+    0x713bd4,
+    0xd95cdb,
+    0xb944dc,
+    0xe345d1,
+    0xbf1fdb,
+    0xaf0ecb
+];
+
+function getRandomColor()
+{
+    return colors[Math.floor(Math.random()*colors.length)];
+}
+
 function selectColor (colorNum, colors)
 {
     if (colors < 1) colors = 1;
     //return 'hsl(' + (colorNum * (360 / colors) % 360) + ',100%,50%)';
-    var color = Phaser.Display.Color.HSVToRGB(colorNum/colors, 1, 0.5);
+    var color = Phaser.Display.Color.HSVToRGB(colorNum/colors, 0.9, 1);
     return (color.r << 16) + (color.g << 8) + color.b;
 }
